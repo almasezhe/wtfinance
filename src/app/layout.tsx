443fbf1a9c90d77abfcc0f721895472d38caa666
Almas <assetalmas07@gmail.com>
2025-02-13 16:01:25 +0500

    0.99.1v Adsense 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Script from "next/script";
-
+import GoogleAdsense from "./googleadsense";
 export const metadata: Metadata = {
   title: "LoverTest.AI",
   description: "Analyze your love journey with AI! Upload chat history, get relationship insights, personalized love advice, and even generate a romantic love story. Discover your compatibility and keep your love alive! 💞",
@@ -14,16 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* Подключение Google AdSense в <head> */}
-        <Script
-          async
-          strategy="afterInteractive"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9740683189212798"
-          crossOrigin="anonymous"
-        />
-      </head>
       <body>{children}</body>
+      <GoogleAdsense pId="9740683189212798" />
+
     </html>
   );
 }
